c05241f9e5b4937b0dcfa283c5adbc426fbcadda
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-04-30 16:41:30 +0530

    Tween Animation Builder

--- lesson_1/lib/shared/screenTitle.dart
+++ lesson_1/lib/shared/screenTitle.dart
@@ -3,13 +3,20 @@ import 'package:flutter/material.dart';
 class ScreenTitle extends StatelessWidget {
   final String? text;
 
-  const ScreenTitle({ this.text});
+  const ScreenTitle({this.text});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text ?? "",
-      style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
-    );
+    return TweenAnimationBuilder(
+        duration: Duration(seconds: 1),
+        tween: Tween<double>(begin: 0, end: 1),
+        child: Text(
+          text ?? "",
+          style: TextStyle(
+              fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        builder: (context, val, child) {
+          return Opacity(opacity: val, child: child!);
+        });
   }
 }
