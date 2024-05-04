2ac666cf87b4eeedb4cc64f7a009dfb7c41aadc6
sushil <baymax732001@gmail.com>
2024-05-04 20:56:28 +0530

    added hear fade animation(explicit animation)

--- lesson_1/lib/shared/heart.dart
+++ lesson_1/lib/shared/heart.dart
@@ -8,25 +8,47 @@ class Heart extends StatefulWidget {
 
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   late AnimationController _animation;
+  late Animation<Color?> _colorAnimation;
+
   @override
   void initState() {
     super.initState();
-    _animation = AnimationController(duration: Durations.medium2, vsync: this);
+    _animation =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+    _colorAnimation =
+        ColorTween(begin: Colors.grey, end: Colors.red).animate(_animation);
     _animation.forward();
     _animation.addListener(() {
+      // setState(() {});
       log("Animation ${_animation.value} ");
     });
   }
 
   @override
+  void dispose() {
+    _animation.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
-    );
+    return AnimatedBuilder(
+        animation: _colorAnimation,
+        builder: (context, child) {
+          return IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: _colorAnimation.value ?? Colors.grey[400],
+              size: 30,
+            ),
+            onPressed: () {
+              if (_animation.status == AnimationStatus.completed) {
+                _animation.reverse();
+              } else {
+                _animation.forward();
+              }
+            },
+          );
+        });
   }
 }
