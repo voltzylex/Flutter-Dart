18abeaa0c93a853f6b926dc5a2b04b97acaa4090
sushil <baymax732001@gmail.com>
2024-05-04 20:45:50 +0530

    learning animation controller

--- lesson_1/lib/shared/heart.dart
+++ lesson_1/lib/shared/heart.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 
 class Heart extends StatefulWidget {
@@ -5,7 +6,18 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  late AnimationController _animation;
+  @override
+  void initState() {
+    super.initState();
+    _animation = AnimationController(duration: Durations.medium2, vsync: this);
+    _animation.forward();
+    _animation.addListener(() {
+      log("Animation ${_animation.value} ");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return IconButton(
