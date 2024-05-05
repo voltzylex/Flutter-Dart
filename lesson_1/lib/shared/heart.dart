2e8f47745e9c34635d688c4601771346c7d2201c
sushil <baymax732001@gmail.com>
2024-05-05 16:32:54 +0530

    learned about add animation status and changed the condition based on it

--- lesson_1/lib/shared/heart.dart
+++ lesson_1/lib/shared/heart.dart
@@ -9,18 +9,31 @@ class Heart extends StatefulWidget {
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   late AnimationController _animation;
   late Animation<Color?> _colorAnimation;
-
+  bool isFav = false;
   @override
   void initState() {
     super.initState();
     _animation =
         AnimationController(duration: Duration(milliseconds: 500), vsync: this);
-    _colorAnimation =
-        ColorTween(begin: Colors.grey, end: Colors.red).animate(_animation);
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
+        .animate(_animation);
     _animation.forward();
     _animation.addListener(() {
       // setState(() {});
       log("Animation ${_animation.value} ");
+    });
+    _animation.addStatusListener((status) {
+      log("Animation Status ${status}");
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isFav = true;
+        });
+      }
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          isFav = false;
+        });
+      }
     });
   }
 
@@ -42,11 +55,12 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
               size: 30,
             ),
             onPressed: () {
-              if (_animation.status == AnimationStatus.completed) {
-                _animation.reverse();
-              } else {
-                _animation.forward();
-              }
+              // if (_animation.status == AnimationStatus.completed) {
+              //   _animation.reverse();
+              // } else {
+              //   _animation.forward();
+              // }
+              isFav ? _animation.reverse() : _animation.forward();
             },
           );
         });
