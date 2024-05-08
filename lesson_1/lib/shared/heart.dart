2ab74e38e9afadf46e9c034ecce8a96e8f8b2e2e
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-05-08 10:09:43 +0530

    curve animation

--- lesson_1/lib/shared/heart.dart
+++ lesson_1/lib/shared/heart.dart
@@ -11,21 +11,23 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   late AnimationController _animation;
   late Animation<Color?> _colorAnimation;
   late Animation<double> _sizeAnimation;
+  late Animation _curve;
   bool isFav = false;
   @override
   void initState() {
     super.initState();
     _animation = AnimationController(
-        duration: Duration(milliseconds: 1500), vsync: this);
+        duration: Duration(milliseconds: 3000), vsync: this);
     _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
         .animate(_animation);
+    _curve = CurvedAnimation(parent: _animation, curve: Curves.slowMiddle);
     _sizeAnimation = TweenSequence(<TweenSequenceItem<double>>[
       TweenSequenceItem<double>(
         tween: Tween(begin: 30, end: 50),
         weight: 50,
       ),
       ...List.generate(
-          10,
+          1,
           (index) => TweenSequenceItem<double>(
                 tween: Tween(
                     begin: index % 2 == 0 ? 30 : 50,
@@ -40,6 +42,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
       // TweenSequenceItem<double>(tween: Tween(begin: 30, end: 50), weight: 50),
       // TweenSequenceItem<double>(tween: Tween(begin: 50, end: 30), weight: 50),
     ]).animate(_animation);
+
     _animation.forward();
     _animation.addListener(() {
       // setState(() {});
@@ -70,7 +73,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-        animation: _colorAnimation,
+        animation: _curve,
         builder: (context, child) {
           return IconButton(
             icon: Icon(
