a74e3e4367bc50cdc6bae30dd24b3e7c4f2d477a
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-05-24 10:25:09 +0530

    remember to add @riverpod to use generator

--- state_management/lib/riverpod/providers/cart_provider.dart
+++ state_management/lib/riverpod/providers/cart_provider.dart
@@ -1,35 +1,32 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:state_management/riverpod/models/product.dart';
-import 'package:state_management/riverpod/providers/products_provider.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
+  // Notifier<Set<Product>>
   // initial value
   @override
   Set<Product> build() {
-    return {
-      Product(
-        id: 2,
-        title: 'Smartphone',
-        price: 500,
-        image: 'assets/products/drum.png',
-      ),
-    };
+    return {};
   }
 
   // add products
-  addProducts(Product products) {
+
+  void addProducts(Product products) {
     if (!state.contains(products)) {
       state = {...state, products};
     }
   }
 
-  removeProducts(Product product) {
+  void removeProducts(Product product) {
     if (state.contains(product)) {
       state = state.where((element) => element.id != product.id).toSet();
     }
   }
 }
 
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
-  return CartNotifier();
-});
+// final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
+//   return CartNotifier();
+// });
