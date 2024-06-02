751e3bf2de2ab5ab8fdbd12e7c691c03236e6fc0
sushil <baymax732001@gmail.com>
2024-06-02 22:55:39 +0530

    created calculator design

--- calculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ calculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mycalculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+        val  button: Button = findViewById(R.id.b8);
+        button.setOnClickListener{
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        fun onDigit(){
+            Toast.makeText(this,"this is button 7",Toast.LENGTH_SHORT,).show();
+        }
+
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this,"this is button 7",Toast.LENGTH_SHORT,).show();
     }
 }
