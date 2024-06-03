d5a0e4e9231db100cd4b79400d4d024a968e6ec6
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-06-03 10:05:34 +0530

    completed net ninja

--- state_management/lib/riverpod/shared/cart_icon.dart
+++ state_management/lib/riverpod/shared/cart_icon.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_management/config.dart';
+import 'package:state_management/riverpod/providers/cart_provider.dart';
 import 'package:state_management/riverpod/screens/cart/cart_screen.dart';
 
-class CartIcon extends StatelessWidget {
+class CartIcon extends ConsumerWidget {
   const CartIcon({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    prettyPrint(tag: "Cart Icon", value: "build Called", type: DebugType.info);
     return Stack(
       children: [
         IconButton(
@@ -19,14 +23,23 @@ class CartIcon extends StatelessWidget {
         Positioned(
           top: 5,
           left: 5,
-          child: Container(
-            width: 18,
-            height: 18,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.blueAccent,
-            ),
+          child: Consumer(
+            builder: (context, ref, child) {
+              final noOfItemInCart = ref.watch(cartNotifierProvider).length;
+              return Container(
+                width: 18,
+                height: 18,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueAccent,
+                ),
+                child: Text(
+                  noOfItemInCart.toString(),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              );
+            },
           ),
         ),
       ],
