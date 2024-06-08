f554f7dd19a918fc8f56dedbe0da05c05ba33403
sushil <baymax732001@gmail.com>
2024-06-08 22:26:59 +0530

    learned about port and hostname how to use express

--- Riverpod_Ecomly/node_server/index.js
+++ Riverpod_Ecomly/node_server/index.js
@@ -1,2 +1,12 @@
+const express = require("express");
+const app = express();
 console.log("Hello world");
 // node mon -- node monitor
+
+// start the server >> 192.168.0.0
+const hostname = "0.0.0.0";
+const port = 5000;
+app.listen(port, hostname, () => {
+    console.log(`The server is running at http://${hostname}:${port}`);
+})
+// http//0.0.0.0:5000
