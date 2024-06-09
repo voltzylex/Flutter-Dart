721bddc06fa3e548c61bb6a2b14f00ebfa76f195
sushil <baymax732001@gmail.com>
2024-06-09 13:04:01 +0530

    learning crud

--- Riverpod_Ecomly/node_server/index.js
+++ Riverpod_Ecomly/node_server/index.js
@@ -1,12 +1,35 @@
 const express = require("express");
+require("dotenv").config(); // Load environment variables from .env file
 const app = express();
-console.log("Hello world");
-// node mon -- node monitor
 
-// start the server >> 192.168.0.0
-const hostname = "0.0.0.0";
-const port = 5000;
-app.listen(port, hostname, () => {
+console.log("Hello world");
+
+// Use default values if environment variables are not set
+const hostname = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
+
+console.log(`Host name is ${hostname} and port name is ${port}`);
+
+app.listen(port, hostname, (err) => {
+    if (err) {
+        console.error('Failed to start server:', err);
+        process.exit(1); // Exit the process with a failure code
+    }
     console.log(`The server is running at http://${hostname}:${port}`);
-})
-// http//0.0.0.0:5000
+});
+// get
+// Create a GET endpoint
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+// C R U D
+
+// CREATE DATA
+app.post()
+// READ THE DATA
+app.get()
+// UPDATE THE DATA
+app.put()
+// DELETE THE DATA
+app.delete()
