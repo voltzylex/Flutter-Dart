6865b40058b742950851c8e4ef6e578d943bb0a2
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-06-22 20:29:14 +0530

    two sum using map

--- DSA/two_sum.dart
+++ DSA/two_sum.dart
@@ -34,3 +34,21 @@ class Solution {
     return sol;
   }
 }
+// Better Approach using hash map
+// Brute Force
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+Map<int,int> map = {};
+  for (int i = 0; i < nums.length; i++) {
+    int num = nums[i];
+    int complement = target - num;
+
+    if (map.containsKey(complement)) {
+      return [map[complement]!, i];
+    }
+
+    map[num] = i;
+  }
+  return [];
+}
+}
