963ea09bda73a6234898622d0df63f1977a4c6be
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-06-22 20:19:50 +0530

    two sum with better version of brute force

--- DSA/two_sum.dart
+++ DSA/two_sum.dart
@@ -16,3 +16,21 @@ class Solution {
     return sol;
   }
 }
+// Brute Force v2
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    List<int> sol = [];
+    for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            // if(i==j){
+            //     continue;
+            // }
+            if(nums[i]+nums[j]==target){
+                sol.addAll([i,j]);
+              return sol;
+            }
+        }
+    }
+    return sol;
+  }
+}
