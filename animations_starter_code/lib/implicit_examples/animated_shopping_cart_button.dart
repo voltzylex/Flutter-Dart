e015f9a24d38d835372a76f5097526a0ee355e0d
sushil <baymax732001@gmail.com>
2024-06-23 13:41:41 +0530

    chaged it to much optimized way

--- animations_starter_code/lib/implicit_examples/animated_shopping_cart_button.dart
+++ animations_starter_code/lib/implicit_examples/animated_shopping_cart_button.dart
@@ -11,45 +11,46 @@ class ShoppingCartButton extends StatelessWidget {
       ),
       body: Center(
         child: Builder(builder: (context) {
-          final ValueNotifier<double> width = ValueNotifier(80);
-          final ValueNotifier<Color> shoppingColor = ValueNotifier(Colors.blue);
-          final ValueNotifier<double> radius = ValueNotifier(10);
+          // final ValueNotifier<double> width = ValueNotifier(80);
+          // final ValueNotifier<Color> shoppingColor = ValueNotifier(Colors.blue);
+          // final ValueNotifier<double> radius = ValueNotifier(10);
+          final ValueNotifier<bool> isExpanded = ValueNotifier(false);
+
           return InkWell(
             onTap: () {
-              shoppingColor.value = Colors.green;
-              radius.value = 30;
-              width.value = 180;
+              // shoppingColor.value = Colors.green;
+              // radius.value = 30;
+              // width.value = 180;
+              isExpanded.value = !isExpanded.value;
             },
-            child: ValueListenableBuilder<double>(
-                valueListenable: width,
+            child: ValueListenableBuilder<bool>(
+                valueListenable: isExpanded,
                 builder: (context, value, child) {
                   return AnimatedContainer(
-                    duration: const Duration(seconds: 1),
-                    width: width.value,
-                    height: 60.0,
-                    decoration: BoxDecoration(
-                      color: shoppingColor.value,
-                      borderRadius: BorderRadius.circular(radius.value),
-                    ),
-                    child: radius.value == 30
-                        ? Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.done,
-                                color: Colors.white,
-                              ),
-                              Text(
-                                " Added to cart",
-                                style: TextStyle(color: Colors.white),
-                              )
-                            ],
-                          )
-                        : const Icon(
-                            Icons.shopping_cart,
+                      duration: const Duration(seconds: 1),
+                      width: isExpanded.value ? 200 : 80,
+                      height: 60.0,
+                      decoration: BoxDecoration(
+                        color: isExpanded.value ? Colors.green : Colors.blue,
+                        borderRadius:
+                            BorderRadius.circular(isExpanded.value ? 30 : 10),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            !isExpanded.value
+                                ? Icons.done
+                                : Icons.shopping_cart,
                             color: Colors.white,
                           ),
-                  );
+                          if (isExpanded.value)
+                            Text(
+                              " Added to cart",
+                              style: TextStyle(color: Colors.white),
+                            )
+                        ],
+                      ));
                 }),
           );
         }),
