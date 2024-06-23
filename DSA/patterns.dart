2ed9c3c62d5af5e83ad8c08a3067d31af6c9ea74
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-06-23 14:33:47 +0530

    pattern 2,3

--- DSA/patterns.dart
+++ DSA/patterns.dart
@@ -18,3 +18,40 @@ void main() {
   int n = 5; // You can change this value to test with different inputs
   nForest(n);
 }
+
+/*
+* 
+* *
+* * *
+*/
+void nForest(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <=i; j++) {
+      print("* ");
+    }
+    print('\n');
+  }
+}
+
+void main() {
+  // Example usage
+  int n = 5; // You can change this value to test with different inputs
+  nForest(n);
+}
+
+/*
+1
+12
+123
+*/
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print(j+1+" " );
+            }
+            System.out.println();
+        }
+    }
+}
