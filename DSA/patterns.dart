94a07b94100235e89b1b345cdd3c16134ebd3082
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-06-25 10:21:01 +0530

    *  *** *****

--- DSA/patterns.dart
+++ DSA/patterns.dart
@@ -113,3 +113,27 @@ public class Solution {
         }
     }
 }
+/*
+  *
+ ***
+*****
+*/
+ class Solution {
+    patter7(){
+        // Write your code here
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n-i-1;j++){
+               print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                print("*");
+            }
+            for(int j = 0;j<n-i-1;j++){
+                print(" ");
+            }
+            print("\n");
+        }
+       
+    }
+    
+}
