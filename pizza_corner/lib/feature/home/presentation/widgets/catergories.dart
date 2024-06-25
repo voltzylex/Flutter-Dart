75b6d6c35a3a2c8454d4092d2a93e2eb8a52d1cf
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-06-25 12:07:28 +0530

    added categories section

--- pizza_corner/lib/feature/home/presentation/widgets/catergories.dart
+++ pizza_corner/lib/feature/home/presentation/widgets/catergories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pizza_corner/core/assets.dart';
+import 'package:pizza_corner/core/const.dart';
 
 class Catergories extends StatefulWidget {
   const Catergories({super.key});
@@ -8,6 +10,7 @@ class Catergories extends StatefulWidget {
 }
 
 class _CatergoriesState extends State<Catergories> {
+  ValueNotifier<double> index = ValueNotifier(0.0);
   List<String> items = ['Popular', 'Pizza', 'Top Rated', 'Pasta', 'Food'];
   @override
   Widget build(BuildContext context) {
@@ -17,22 +20,40 @@ class _CatergoriesState extends State<Catergories> {
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: items.length,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Column(
-            children: [
-              AnimatedDefaultTextStyle(
-                style: const TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                    fontFamily: 'Nunito'),
-                duration: Durations.long2,
-                child: Text(
-                  items.elementAt(index),
-                ),
-              ),
-            ],
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () => this.index.value = index.toDouble(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: ValueListenableBuilder(
+                valueListenable: this.index,
+                builder: (context, value, _) {
+                  bool isSelected = value == index;
+                  return Column(
+                    children: [
+                      AnimatedDefaultTextStyle(
+                        duration: Durations.medium2,
+                        style: TextStyle(
+                            fontSize: isSelected ? 21 : 19,
+                            color:
+                                isSelected ? Colors.amber[700] : Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: nunito),
+                        child: Text(
+                          items.elementAt(index),
+                        ),
+                      ),
+                      AnimatedContainer(
+                        duration: Durations.medium2,
+                        height: 5.0,
+                        width: isSelected ? 20 : 0,
+                        decoration: BoxDecoration(
+                          color: accentColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ],
+                  );
+                }),
           ),
         ),
       ),
