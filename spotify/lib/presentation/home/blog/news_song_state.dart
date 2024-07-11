af314665051d9ebd540e08a7ed7c457324291f80
sushil <baymax732001@gmail.com>
2024-07-11 09:14:04 +0530

    added states for songs

--- spotify/lib/presentation/home/blog/news_song_state.dart
+++ spotify/lib/presentation/home/blog/news_song_state.dart
@@ -1,1 +1,13 @@
+import '../../../domain/entities/song/song_entity.dart';
 
+abstract class NewsSongState {}
+
+class NewSongLoading extends NewsSongState {}
+
+class NewSongLoaded extends NewsSongState {
+  final List<SongEntity> songs;
+
+  NewSongLoaded({required this.songs});
+}
+
+class NewSongLoadFailure extends NewsSongState {}
