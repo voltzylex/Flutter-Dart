b0ff4b30bd15530e9b6d371e779eb68d4fe54456
sushil <baymax732001@gmail.com>
2024-07-11 10:00:20 +0530

    added cubit for api calling

---
+++ spotify/lib/presentation/home/blog/news_song_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify/domain/usecases/song/get_new_songs.dart';
+import 'package:spotify/presentation/home/blog/news_song_state.dart';
+
+import '../../../service_locator.dart';
+
+class SongCubit extends Cubit<NewsSongState> {
+  // super will whenever the cubit is initialized
+  SongCubit() : super(NewSongLoading());
+
+  Future<void> getNewSongs() async {
+    final returnedSongs = await sl<GetNewSongUsecase>().call();
+    returnedSongs.fold(
+      (l) {
+        return emit(NewSongLoadFailure());
+      },
+      (r) {
+        return emit(NewSongLoaded(songs: r));
+      },
+    );
+  }
+}
---
+++ spotify/lib/presentation/home/widgets/new_songs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewSongs extends StatelessWidget {
+  const NewSongs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
